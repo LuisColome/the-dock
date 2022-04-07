5cb51938f70455edee6e436b60e42843165be0de
Luis Colomé <hello@luiscolome.com>
2022-04-07 12:10:34 +0200

    New style for WP login screen

--- inc/login-logo.php
+++ inc/login-logo.php
@@ -31,6 +31,45 @@ function ea_login_logo() {
 	$logo = get_stylesheet_directory_uri() . $logo_path;
     ?>
     <style type="text/css">
+    body.login {
+        display: grid;
+        place-items: center;
+        background: hsl(212, 100%, 91%);
+        background: linear-gradient(
+            180deg,
+            hsl(212, 100%, 91%) 11%,
+            hsl(212, 66%, 75%) 100%
+        );
+        height: 100vh;
+        overflow: hidden;
+    }
+    body.login::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: hsla(240, 3%, 94%, 1);
+        width: 200%;
+        height: 200%;
+        transform: rotate(16deg) translateX(-95%) translateY(-15%);
+        z-index: 0;
+        -webkit-box-shadow: 5px 0px 0px 8px rgba(240, 240, 241, 0.5);
+        box-shadow: 5px 0px 0px 8px rgba(240, 240, 241, 0.5);
+    }
+    @media (min-width:400px) {
+        body.login::before {
+            transform: rotate(16deg) translateX(-90%) translateY(-15%);
+        }
+    }
+    @media (min-width:660px) {
+        body.login::before {
+            transform: rotate(16deg) translateX(-80%) translateY(-15%);
+        }
+    }
+    #login {
+        position: relative;
+        z-index: 9;
+    }
     .login form {
         border: none;
         border-radius: 10px;
